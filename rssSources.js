7d245dd1c30d81a52a24a140cf314143e0793819
hashamdanish12-afk <hashamdanish12@gmail.com>
2025-10-18 20:08:50 +0500

    Update rssSources.js

--- rssSources.js
+++ rssSources.js
@@ -1,29 +1,22 @@
-module.exports = {
+// rssSources.js
+const rssSources = {
   global: [
-    { name: 'Reuters', url: 'https://www.reuters.com/rssFeed/worldNews' },
-    { name: 'BBC', url: 'https://feeds.bbci.co.uk/news/world/rss.xml' },
-    { name: 'NYTimes', url: 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml' }
-  ],
-  pakistan: [
-    { name: 'Dawn', url: 'https://www.dawn.com/feeds/home' },
-    { name: 'Geo', url: 'https://www.geo.tv/rss' }
-  ],
-  usa: [
-    { name: 'Reuters', url: 'https://www.reuters.com/rssFeed/usNews' },
-    { name: 'ESPN', url: 'https://www.espn.com/espn/rss/news' }
-  ],
-  uk: [
-    { name: 'BBC UK', url: 'https://feeds.bbci.co.uk/news/uk/rss.xml' }
-  ],
-  india: [
-    { name: 'The Hindu', url: 'https://www.thehindu.com/?service=rss' }
-  ],
-  genz: [
-    { name: 'Reddit GenZ', url: 'https://www.reddit.com/r/GenZedong/.rss' },
-    { name: 'PopBase Twitter via RSSHub', url: 'https://rsshub.app/twitter/user/PopBase' }
+    { name: "BBC World", url: "https://feeds.bbci.co.uk/news/world/rss.xml" },
+    { name: "Reuters", url: "https://feeds.reuters.com/reuters/topNews" },
+    { name: "Al Jazeera", url: "https://www.aljazeera.com/xml/rss/all.xml" },
+    { name: "The Guardian", url: "https://www.theguardian.com/world/rss" },
+    { name: "CNN", url: "http://rss.cnn.com/rss/edition_world.rss" }
   ],
   trending: [
-    { name: 'Reddit r/popular', url: 'https://www.reddit.com/r/popular/.rss' },
-    { name: 'Reddit trending', url: 'https://www.reddit.com/r/all/.rss' }
+    { name: "TechCrunch", url: "https://techcrunch.com/feed/" },
+    { name: "The Verge", url: "https://www.theverge.com/rss/index.xml" },
+    { name: "Reddit /r/worldnews", url: "https://www.reddit.com/r/worldnews/.rss" },
+    { name: "NY Times - World", url: "https://rss.nytimes.com/services/xml/rss/nyt/World.xml" }
+  ],
+  genz: [
+    { name: "Mashable", url: "https://mashable.com/feed/" },
+    { name: "BuzzFeed News", url: "https://www.buzzfeed.com/world.xml" }
   ]
 };
+
+export default rssSources;
